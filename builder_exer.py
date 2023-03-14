825dc882b2965832ac1d815b24fd859334f52517
Breno Dantas <39684269+brenossa@users.noreply.github.com>
2023-03-14 17:02:20 -0300

    Update builder_exer.py

--- builder_exer.py
+++ builder_exer.py
@@ -1,35 +1,44 @@
+# Pizza base
 class Pizza:
+    # Define os atributos da pizza
     def __init__(self, tamanho, crosta, adicionais):
         self._tamanho = tamanho
         self._crosta = crosta
         self._adicionais = adicionais
     
+    # String que deve ser retornada após a construção da pizza
     def __str__(self):
         return f"Pizza de tamanho {self._tamanho} e crosta {self._crosta} com os seguintes adicionais: {', '.join(self._adicionais)}"
 
+# Construtor de Pizza: Constrói uma pizza do zero com os atributos selecionados pelo cliente
 class ConstrutorDePizza:
+    # Inicia definindo as variáveis vazias
     def __init__(self):
         self._tamanho = None
         self._crosta = None
         self._adicionais = []
     
+    # Função para definir o tamanho da pizza
     def define_tamanho(self, tamanho):
         self._tamanho = tamanho
     
+    # Função para definir a crosta da pizza
     def define_crosta(self, crosta):
         self._crosta = crosta
 
+    # Função para criar um novo adicional a pizza
     def novo_adicional(self, adicional):
         self._adicionais.append(adicional)
     
+    # Função para Construir a pizza com todas as variáveis definidas
     def construir(self):
         return Pizza(self._tamanho, self._crosta, self._adicionais)
 
-construtor = ConstrutorDePizza()
+construtor = ConstrutorDePizza() # Objeto da classe construtora
 construtor.define_tamanho("larga")
 construtor.define_crosta("grossa")
 construtor.novo_adicional("carne seca")
 construtor.novo_adicional("mussarela")
 construtor.novo_adicional("molho de tomate")
-pizza = construtor.construir()
+pizza = construtor.construir() # Objeto Pizza gerado da classe construtora
 print(pizza)
